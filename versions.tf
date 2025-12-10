13f1242a9ec784689b91cb665a2a72d44e52b9d4
jack-the-snake <143336469+jack-the-snake@users.noreply.github.com>
2025-12-10 14:49:45 +0000

    state

--- versions.tf
+++ versions.tf
@@ -1,8 +1,12 @@
 terraform {
+  required_version = "~>1.14.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "6.25.0"
     }
+  }
+  backend "s3" {
+    use_lockfile = true
   }
 }
