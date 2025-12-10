c19b767ef55ec55e128ec2cd3377567c1732b6d0
jack-the-snake <143336469+jack-the-snake@users.noreply.github.com>
2025-12-10 12:53:18 +0000

    public bucket

--- main.tf
+++ main.tf
@@ -26,3 +26,40 @@ resource "aws_s3_object" "object" {
   key    = basename(each.key)
   source = each.key
 }
+
+resource "aws_s3_bucket_policy" "allow_public_access" {
+  depends_on = [aws_s3_bucket_public_access_block.public_access]
+
+  bucket = aws_s3_bucket.my-bucket.id
+  policy = data.aws_iam_policy_document.allow_public_access.json
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.my-bucket.id
+
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+data "aws_iam_policy_document" "allow_public_access" {
+  statement {
+    sid    = "PublicReadGetObject"
+    effect = "Allow"
+    principals {
+      identifiers = ["*"]
+      type        = "*"
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.my-bucket.arn,
+      "${aws_s3_bucket.my-bucket.arn}/*",
+    ]
+  }
+}
