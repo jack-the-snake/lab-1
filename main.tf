ab50e834812deed826102e0b4eac968ef8bc2d01
jack-the-snake <143336469+jack-the-snake@users.noreply.github.com>
2025-12-17 08:33:28 +0000

    Basic data block with http provider

--- main.tf
+++ main.tf
@@ -1,19 +1,41 @@
-provider "aws" {
-  # Configuration options
-  ignore_tags {
-    keys = var.tags-to-ignore
+terraform {
+  required_version = "~>1.14.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.25.0"
+    }
+    http = {
+      source = "hashicorp/http"
+      version = "3.5.0"
+    }
   }
 }
 
-resource "aws_s3_bucket" "my-bucket" {
-  bucket_prefix = "awsninja20-"
+
+provider "aws" {
+
 }
 
-resource "aws_s3_object" "object" {
-  for_each = fileset(path.module, "messages/*")
-
-  bucket = aws_s3_bucket.my-bucket.bucket
-  key    = basename(each.key)
-  source = each.key
+provider "http" {
+  # Configuration options
 }
+
+
+data "http" "terraform_version" {
+  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+}
+
+locals {
+  terraform_details = jsondecode(data.http.terraform_version.response_body)
+}
+
+
+# Terraform 1.14.2 (1765457324)
+output "info" {
+  value = "Terraform ${local.terraform_details.current_version} (${local.terraform_details.current_release})"
+}
+
+
+
 
--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "public-access" {
-  type    = bool
-  default = false
-}
-
-variable "tags-to-ignore" {
-  type    = list(string)
-  default = ["department"]
-}
